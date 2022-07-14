a9cc00ba8197b287f1f6219ed483cdc6f2f7f89d
KONCHA SIVA KARTHIK REDDY <90611447+sivakarthikreddy12@users.noreply.github.com>
2022-07-14 10:56:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import cv2
-import mediapipe as mp
-from math import hypot
-from ctypes import cast, POINTER
-from comtypes import CLSCTX_ALL
-from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
-import numpy as np
+
 import cv2
 import mediapipe as mp
 from math import hypot
